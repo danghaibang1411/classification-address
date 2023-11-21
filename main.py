57421932de835fc65d1db127591ad2ef4ea8d487
bangdang <bang.dang.hai1411@gmail.com>
2023-11-22 01:46:13 +0700

    update

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ abbreviation_mapping = {
     'tỉnh': 't',
     'huyện': 'h',
     'quận': 'q',
+    'phuong': 'p',
     'thị trấn': 'tt',
-    'thành phố 1': 'pho',
-    'thành phố 2': 'thanh',
     'a1': 'huyen',
     'a2': 'thanhpho',
     'a3': 'quan',
     'a4': 'tinh',
     'a5': 'thitran',
+    'a9': 'thi tran',
     'a6': 'thi',
     'a7': 'tran',
+    'a8': 'ho',
+    'a10': 'thanh pho',
+    'a11': 'phuong',
+    'a12': 'tx',
+    'a13': 'thi xa',
+    'empty': '',
+    'a14': 'khupho',
+    'a15': 'khu',
+    'a16': 'xa',
 }
 
 with open('data/cities.json', 'r', errors="ignore", encoding="utf-8") as file:
@@ -64,18 +73,24 @@ def main():
             result_list_districts.clear()
             result_list_wards.clear()
             data = line_data.get("text")
-            result_array = [token.strip() for token in re.split(r'[,\s]+', data)]
+            result_array = [token.strip() for token in re.split(r'[A-Z][a-z]*(?<=[a-z])(?=[A-Z])|[,\s.]+', data)]
             result_array = all_lower(result_array)
             final = []
             for ss in result_array:
                 final.append(re.sub('<.*?>', '', remove_regex(ss).replace(".", "")))
+                for word in final:
+                    if word in [abbr.lower() for abbr in abbreviation_mapping.values()] or (
+                            not word.isdigit() and len(word) == 1):
+                        final.remove(word)
             i = len(final) - 1
 
             # Code for cities
-            is_use_second_word, result_for_cities = get_result_list_for_cities(final[i], final[i-1], list_province)
+            if i < 0:
+                continue
+            is_use_second_word, result_for_cities = get_result_list_for_cities(final[i], final[i - 1], list_province)
             ret = len(result_for_cities)
             if is_use_second_word:
-                text = final[i-1] + final[i]
+                text = final[i - 1] + final[i]
                 i = i - 1
             else:
                 text = final[i]
@@ -86,14 +101,20 @@ def main():
                 result_for_cities_again = get_result_list_for_again(text, list_province)
                 ret = len(result_for_cities_again)
             i = i - 1
+            if i < 0:
+                continue
             if len(result_for_cities_again) > 0:
                 result_for_cities = result_for_cities_again
 
             if len(result_for_cities) > 0:
-                if final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
+                while final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
                     i = i - 1
-                    if final[i] == "thanh":
-                        i = i - 1
+                    if i < 0:
+                        continue
+                while final[i - 1] + final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
+                    i = i - 2
+                    if i < 0:
+                        continue
                 name_cities = result_for_cities[0].get('name')
             for each_name_cities in result_for_cities:
                 code = each_name_cities.get("code")
@@ -102,26 +123,39 @@ def main():
                         result_list_districts.append(list_dis)
 
             # Code for districts
-            is_use_second_word, result_for_districts = get_result_list_for_cities(final[i], final[i - 1], result_list_districts)
+            is_use_second_word, result_for_districts = get_result_list_for_cities(final[i], final[i - 1],
+                                                                                  result_list_districts)
             ret = len(result_for_districts)
             if is_use_second_word:
                 text = final[i - 1] + final[i]
                 i = i - 1
+                if i < 0:
+                    continue
             else:
                 text = final[i]
             result_for_districts_again = []
             if i > 0 and ret > 5:
                 i = i - 1
+                if i < 0:
+                    continue
                 text = final[i] + text
                 result_for_districts_again = get_result_list_for_again(text, result_list_districts)
                 ret = len(result_for_districts_again)
             i = i - 1
+            if i < 0:
+                continue
             if len(result_for_districts_again) > 0:
                 result_for_districts = result_for_districts_again
 
             if len(result_for_districts) > 0:
-                if final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
+                while final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
                     i = i - 1
+                    if i < 0:
+                        continue
+                while final[i - 1] + final[i] in [abbr.lower() for abbr in abbreviation_mapping.values()]:
+                    i = i - 2
+                    if i < 0:
+                        continue
                 name_districts = result_for_districts[0].get('name')
             for each_name_district in result_for_districts:
                 code = each_name_district.get("code")
@@ -130,7 +164,7 @@ def main():
                         result_list_wards.append(list_ward)
 
             # Code for wards
-            is_use_second_word, result_for_wards = get_result_list_for_cities(final[i], final[i - 1], result_list_wards)
+            is_use_second_word, result_for_wards = get_result_list_for_wards(final[i], final[i - 1], result_list_wards)
             ret = len(result_for_wards)
             if is_use_second_word:
                 text = final[i - 1] + final[i]
@@ -147,6 +181,9 @@ def main():
             if len(result_for_wards_again) > 0:
                 result_for_wards = result_for_wards_again
 
+            if len(result_for_wards) > 1:
+                is_bool, result_for_wards = get_result_list_for_cities(text, "", result_list_wards)
+
             if len(result_for_wards) == 1:
                 arr = result_for_wards[0].get('path').split(', ')
                 name_cities = arr[2]
@@ -157,9 +194,9 @@ def main():
 
         finally:
             expected_result = line_data.get('result')
-            if (name_cities == expected_result.get('province') or name_districts == expected_result.get('district')
-                    or name_wards == expected_result.get(
-                    'ward')):
+            if (name_cities == expected_result.get('province') and name_districts == expected_result.get('district')
+                    and name_wards == expected_result.get(
+                        'ward')):
                 correct_record = correct_record + 1
             export_to_json('data/output.json', data, expected_result, name_cities, name_districts, name_wards)
 
@@ -187,26 +224,65 @@ def get_result_list_for_cities(first_word, second_word, list_data_add):
     result = []
     result_one_word = []
     result_two_word = []
-    if len(first_word) < 1 or len(first_word+second_word) < 1:
+    if len(first_word) < 1 or len(first_word + second_word) < 1:
         return is_use_second_word, result
     for ref in list_data_add:
         cities = ref.get("slug").replace("-", "")
         min_same = min(len(first_word), len(cities))
         same = LCSubStr(first_word, cities, len(first_word), len(cities))
-        if (same / min_same) >= 0.65:
-            result_one_word.append(ref)
+        if (same / min_same) >= 0.7:
+            result_one_word.append({'data': ref, 'weight': same / min_same})
 
-    double_word = second_word+first_word
+    double_word = second_word + first_word
     for ref in list_data_add:
         cities = ref.get("slug").replace("-", "")
         min_same = min(len(double_word), len(cities))
         same = LCSubStr(double_word, cities, len(double_word), len(cities))
-        if (same / min_same) >= 0.65:
+        if (same / min_same) >= 0.7:
+            result_two_word.append({'data': ref, 'weight': same / min_same})
+
+    if 0 < len(result_two_word) < len(result_one_word) or (len(result_two_word) > 0 and len(result_one_word) == 0) \
+            or (len(result_two_word) == len(result_one_word) and len(first_word) < 7) and len(result_one_word) > 0:
+        is_use_second_word = True
+        result.append(max(result_two_word, key=lambda x: x['weight']).get('data'))
+    elif len(result_two_word) == 0 and len(result_one_word) == 0:
+        is_use_second_word = False
+        result = []
+    else:
+        is_use_second_word = False
+        result.append(max(result_one_word, key=lambda x: x['weight']).get('data'))
+    return is_use_second_word, result
+
+
+def get_result_list_for_wards(first_word, second_word, list_data_add):
+    is_use_second_word = False
+    result = []
+    result_one_word = []
+    result_two_word = []
+    if len(first_word) < 1 or len(first_word + second_word) < 1:
+        return is_use_second_word, result
+    for ref in list_data_add:
+        cities = ref.get("slug").replace("-", "")
+        min_same = min(len(first_word), len(cities))
+        same = LCSubStr(first_word, cities, len(first_word), len(cities))
+        if (same / min_same) >= 0.7:
+            result_one_word.append(ref)
+
+    double_word = second_word + first_word
+    for ref in list_data_add:
+        cities = ref.get("slug").replace("-", "")
+        min_same = min(len(double_word), len(cities))
+        same = LCSubStr(double_word, cities, len(double_word), len(cities))
+        if (same / min_same) >= 0.7:
             result_two_word.append(ref)
 
-    if len(result_two_word) < len(result_one_word):
+    if 0 < len(result_two_word) < len(result_one_word) or (len(result_two_word) > 0 and len(result_one_word) == 0) \
+            or (len(result_two_word) == len(result_one_word) and len(first_word) < 7) and len(result_one_word) > 0:
         is_use_second_word = True
         result = result_two_word
+    elif len(result_two_word) == 0 and len(result_one_word) == 0:
+        is_use_second_word = False
+        result = []
     else:
         is_use_second_word = False
         result = result_one_word
